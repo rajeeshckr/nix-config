645200ceac31984055ed446d75d6ebe70c857006
sam <sam@samlockart.com>
2024-11-12 20:48:52 +1100

    add default stateversion

--- nixos/desktop/configuration.nix
+++ nixos/desktop/configuration.nix
@@ -155,5 +155,9 @@
   services.smartd = {
     enable = true;
   };
+
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
 }
 
