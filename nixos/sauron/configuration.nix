3f4c9f71ad2a9bf11ea76d8a00bafb3e36a66279
sam <sam@samlockart.com>
2025-01-29 12:03:13 +1100

    open up

--- nixos/sauron/configuration.nix
+++ nixos/sauron/configuration.nix
@@ -97,14 +97,15 @@
   services.ollama = {
     enable = true;
     port = 11434;
+    host = "0.0.0.0";
     acceleration = "cuda";
+    openFirewall = true;
     loadModels = [
       "deepseek-r1:32b"
     ];
     environmentVariables = {
       OLLAMA_ORIGINS = "http://sauron.middleearth.samlockart.com";
       OLLAMA_DEBUG = "true";
-      OLLAMA_HOST = "http://sauron.middleearth.samlockart.com";
     };
   };
 
