11db1272d0b918391850da4395f165d507842a5b
alam0rt <sam@samlockart.com>
2025-03-30 18:36:04 +1100

    fix args

--- nixos/desktop/vllm.nix
+++ nixos/desktop/vllm.nix
@@ -10,22 +10,19 @@ in {
       preRunExtraOptions = [
         "--runtime"
         "nvidia"
-        "--gpus"
-        "all"
       ];
       extraOptions = [
         "--ipc=host"
+        "--device" "nvidia.com/gpu=all"
       ];
       cmd = [
-        "serve"
-        "mistralai/Mistral-Small-3.1-24B-Instruct-2503"
+        "--model" "mistralai/Mistral-Small-3.1-24B-Instruct-2503"
         "--tokenizer_mode" "mistral"
         "--config_format" "mistral"
         "--load_format" "mistral"
         "--tool-call-parser" "mistral"
         "--enable-auto-tool-choice"
         "--limit_mm_per_prompt"
-        "image=10"
         "--tensor-parallel-size" "2"
       ];
       image = "vllm/vllm-openai:latest";
