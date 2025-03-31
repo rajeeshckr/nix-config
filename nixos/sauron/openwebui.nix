df99088b3f36ece92f665086ef617c86938e0c17
alam0rt <sam@samlockart.com>
2025-04-01 07:11:40 +1100

    comment

--- nixos/sauron/openwebui.nix
+++ nixos/sauron/openwebui.nix
@@ -11,8 +11,9 @@ in {
     port = 11111;
     environment = {
       OLLAMA_API_BASE_URL = "http://desktop:11434";
-      OPENAI_API_BASE_URL = "http://desktop:8000";
-      ENABLE_OPENAI_API = "true";
+      # for vllm use
+      # OPENAI_API_BASE_URL = "http://desktop:8000";
+      # ENABLE_OPENAI_API = "true";
       # todo: fill out with more of the settings i've overriden in the ui
     };
   };
