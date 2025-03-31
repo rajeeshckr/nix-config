7a67d9f18460fcc6d3247bc3747c3db4cf958a68
alam0rt <sam@samlockart.com>
2025-04-01 06:58:25 +1100

    ollama unstable with gemma3

--- nixos/config/llm.nix
+++ nixos/config/llm.nix
@@ -4,12 +4,14 @@
   # ollama / LLM
   services.ollama = {
     enable = true;
+    package = pkgs.unstable.ollama-cuda;
     port = 11434;
     host = "0.0.0.0";
     acceleration = "cuda";
     openFirewall = true;
     loadModels = [
-      "deepseek-r1:32b"
+      "deepseek-r1:14b"
+      "gemma3:12b"
     ];
     environmentVariables = {
       OLLAMA_ORIGINS = "http://sauron.middleearth.samlockart.com";
