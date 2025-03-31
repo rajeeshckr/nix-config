7ec129b851b22231fecc0a228141784b058c896a
alam0rt <sam@samlockart.com>
2025-04-01 07:26:46 +1100

    more scrape configs

--- nixos/sauron/monitoring.nix
+++ nixos/sauron/monitoring.nix
@@ -39,16 +39,35 @@ in {
     extraFlags = [ "--collector.ethtool" "--collector.softirqs" "--collector.tcpstat" ];
   };
 
+  services.prometheus.exporters = {
+    zfs.enable = true;
+    nginx.enable = true;
+  };
+
   services.prometheus = {
     enable = true;
     globalConfig.scrape_interval = "10s";
     scrapeConfigs = [
-    {
-      job_name = "node";
-      static_configs = [{
-        targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
-      }];
-    }
+      {
+        job_name = "node";
+        static_configs = [{
+          targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
+        }];
+      }
+
+      {
+        job_name = "zfs";
+        static_configs = [{
+          targets = [ "localhost:${toString config.services.prometheus.exporters.zfs.port}" ];
+        }];
+      }
+
+      {
+        job_name = "nginx";
+        static_configs = [{
+          targets = [ "localhost:${toString config.services.prometheus.exporters.nginx.port}" ];
+        }];
+      }
     ];
   }; 
 }
