0a6459e49ca664e0cbf715c6c8a1575ac8e13ce7
rajeeshckr <rajeesh.ckr@gmail.com>
2025-04-16 09:55:10 +1000

    Nvidia working now

--- nixos/config/nvidia.nix
+++ nixos/config/nvidia.nix
@@ -5,45 +5,40 @@
   };
 
   # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-  };
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = ["modesetting" "nvidia"];
 
+  hardware = {
 
-  ## nvidia
-  # Load nvidia driver for Xorg and Wayland
-  hardware.nvidia = {
+    nvidia = {
+      modesetting.enable = false;
+      powerManagement = {
+        enable = false;
+        finegrained = false;
+      };
+      open = true;
+      nvidiaSettings = true;
+#     package = config.boot.kernelPackages.nvidiaPackages.beta;
 
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = true;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
+  package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "570.133.07";
+    sha256_64bit = "sha256-LUPmTFgb5e9VTemIixqpADfvbUX1QoTT2dztwI3E3CY=";
+    sha256_aarch64 = "sha256-yTovUno/1TkakemRlNpNB91U+V04ACTMwPEhDok7jI0=";
+    openSha256 = "sha256-9l8N83Spj0MccA8+8R1uqiXBS0Ag4JrLPjrU3TaXHnM=";
+    settingsSha256 = "sha256-XMk+FvTlGpMquM8aE8kgYK2PIEszUZD2+Zmj2OpYrzU=";
+    persistencedSha256 = "sha256-G1V7JtHQbfnSRfVjz/LE2fYTlh9okpCbE4dfX9oYSg8=";
   };
+
+#      prime = { 
+#            offload = {
+ #     enable = true;
+  #    enableOffloadCmd = true;
+   # };
+        # Make sure to use the correct Bus ID values for your system!
+ #       nvidiaBusId = "PCI:01:00.0";  # This is the Bus ID for your NVIDIA GPU.
+  #    };
+    };
+  };
+
 }
