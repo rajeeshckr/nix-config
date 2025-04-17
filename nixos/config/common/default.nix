0f9d4a256034e3f89568266c327cdf62185fc2c9
rajeeshckr <rajeesh.ckr@gmail.com>
2025-04-17 21:21:36 +1000

    add zsh

--- nixos/config/common/default.nix
+++ nixos/config/common/default.nix
@@ -10,6 +10,21 @@
   # enable zsh integration with nix
   programs.zsh = {
     enable = true;
+    enableCompletions = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+
+    oh-my-zsh = {
+        enable = true;
+        plugins = [ "git" "thefuck" ];
+        theme = "robbyrussell";
+      };
+      
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch --flake .#nixos";
+    };
+    history.size = 10000;
   };
 
   services.smartd = {
