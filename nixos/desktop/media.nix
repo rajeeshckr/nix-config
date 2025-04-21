c2420f534f4a401633ac41f5c9cb7902cecf22fa
rajeeshckr <rajeesh.ckr@gmail.com>
2025-04-21 21:26:24 +1000

    Update media.nix

--- nixos/desktop/media.nix
+++ nixos/desktop/media.nix
@@ -50,16 +50,16 @@ in {
 
   # selfhosted rarbg
   # https://github.com/mgdigital/rarbg-selfhosted
-  virtualisation.oci-containers.containers = {
-    rarbg = {
-      image = "ghcr.io/mgdigital/rarbg-selfhosted:latest";
-      ports = ["3333:3333"];
-      volumes = ["/srv/data/rarbg_db.sqlite:/rarbg_db.sqlite"];
-    };
+#   virtualisation.oci-containers.containers = {
+#     rarbg = {
+#       image = "ghcr.io/mgdigital/rarbg-selfhosted:latest";
+#       ports = ["3333:3333"];
+#       volumes = ["/srv/data/rarbg_db.sqlite:/rarbg_db.sqlite"];
+#     };
 
-    flaresolverr = {
-      image = "ghcr.io/flaresolverr/flaresolverr:latest";
-      ports = ["8191:8191"];
-    };
-  };
+#     flaresolverr = {
+#       image = "ghcr.io/flaresolverr/flaresolverr:latest";
+#       ports = ["8191:8191"];
+#     };
+#   };
 }
