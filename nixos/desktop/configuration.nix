a30a3b9c5360fbb28e20d754a4ac25499f0cfe99
rajeeshckr <rajeesh.ckr@gmail.com>
2025-05-24 15:30:30 +1000

    Using static IP

--- nixos/desktop/configuration.nix
+++ nixos/desktop/configuration.nix
@@ -30,6 +30,14 @@
 
   networking.firewall.enable = false;
 
+  networking.useDHCP = false;
+  networking.interfaces.enp6s0.ipv4.addresses = [{
+    address = "192.168.1.29";
+    prefixLength = 24;
+  }];
+  networking.defaultGateway = "192.168.1.1";
+  networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
+
   programs.wireshark.enable = true;
   programs.nix-ld.enable = true;
 
