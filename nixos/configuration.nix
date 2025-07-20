4064ffe5a4dcccaae7e17d54fbac4992b37006bd
rajeeshckr <rajeesh.ckr@gmail.com>
2025-07-20 11:38:03 +1000

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -67,6 +67,8 @@
   # allow interacting with secret store
   environment.systemPackages = [ inputs.agenix.packages.x86_64-linux.default ];
 
+  age.identityPaths = [ "/root/.ssh/id_rsa" ];
+
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
