4a0ad231a7ea011804b5ed27447f8f3f2b708cbb
rajeeshckr <rajeesh.ckr@gmail.com>
2025-07-24 21:13:54 +1000

    Update samba.nix

--- nixos/desktop/samba.nix
+++ nixos/desktop/samba.nix
@@ -42,4 +42,24 @@ in
     enable = true;
     openFirewall = true;
   };
+
+  # Mount external SMB share
+  fileSystems."/movies-indian" = {
+    device = "//192.168.1.1/USB_Storage";
+    fsType = "cifs";
+    options = [
+      "guest"             # Use guest account, no password needed
+      "uid=1000"          # Mount as your user (run `id -u raj` to confirm)
+      "gid=986"           # Mount as the 'users' group (run `id -g raj` to confirm)
+      "iocharset=utf8"    # Character set for file names
+      "nofail"            # Don't block boot if the share is unavailable
+      "_netdev"           # This is a network device
+      "x-systemd.automount" # Mount on first access
+    ];
+  };
+
+  # Ensure cifs-utils is available for SMB mounting
+  environment.systemPackages = with pkgs; [
+    cifs-utils
+  ];
 }
