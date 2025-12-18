75f45faacad8995876670fb11bec2957b8549946
rajeeshckr <rajeesh.ckr@gmail.com>
2025-12-18 13:58:36 +1100

    Update llm.nix to use unstable.ollama-cuda and remove gemma3 model

--- nixos/config/llm.nix
+++ nixos/config/llm.nix
@@ -4,14 +4,13 @@
   # ollama / LLM
   services.ollama = {
     enable = true;
-    package = pkgs.ollama-cuda;
+    package = pkgs.unstable.ollama-cuda;
     port = 11434;
     host = "0.0.0.0";
     acceleration = "cuda";
     openFirewall = true;
     loadModels = [
       "deepseek-r1:14b"
-      "gemma3:12b"
     ];
   };
 }
