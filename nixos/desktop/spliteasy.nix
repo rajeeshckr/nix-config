4d9c220ea83749baeed2a366acded1d6ecd25f31
rajeeshckr <rajeesh.ckr@gmail.com>
2026-02-08 16:24:50 +1100

    Add web-client service

--- nixos/desktop/spliteasy.nix
+++ nixos/desktop/spliteasy.nix
@@ -1,11 +1,14 @@
-# SplitEasy backend - expense splitting app
+# SplitEasy - expense splitting app
 #
-# The container image must be built locally first:
-#   podman build -t spliteasy-backend:latest ~/spliteasy/backend
+# Images must be built locally as root first:
+#   sudo podman build -t spliteasy-backend:latest ~/spliteasy/backend
+#   sudo podman build -t spliteasy-webclient:latest ~/spliteasy/web-client
 #
-# After rebuild, the service is managed by systemd:
+# After rebuild, services are managed by systemd:
 #   systemctl status podman-spliteasy-backend
+#   systemctl status podman-spliteasy-webclient
 #   journalctl -u podman-spliteasy-backend -f
+#   journalctl -u podman-spliteasy-webclient -f
 #
 { config
 , lib
@@ -13,8 +16,14 @@
 , ... }:
 let
   cfg = {
-    image = "localhost/spliteasy-backend:latest";
-    port = 9090;
+    backend = {
+      image = "localhost/spliteasy-backend:latest";
+      port = 9090;
+    };
+    webclient = {
+      image = "localhost/spliteasy-webclient:latest";
+      port = 3000;
+    };
     dataDir = "/var/lib/spliteasy";
   };
 in {
@@ -23,18 +32,29 @@ in {
     "d ${cfg.dataDir} 0755 root root -"
   ];
 
-  virtualisation.oci-containers.containers.spliteasy-backend = {
-    autoStart = true;
-    image = cfg.image;
-    extraOptions = [
-      "--pull=never"  # image is built locally, don't try to pull from a registry
-    ];
-    ports = ["${toString cfg.port}:8080"];
-    volumes = [
-      "${cfg.dataDir}:/app/data:Z"
-    ];
+  virtualisation.oci-containers.containers = {
+    spliteasy-backend = {
+      autoStart = true;
+      image = cfg.backend.image;
+      extraOptions = [
+        "--pull=never"  # image is built locally, don't try to pull from a registry
+      ];
+      ports = ["${toString cfg.backend.port}:8080"];
+      volumes = [
+        "${cfg.dataDir}:/app/data:Z"
+      ];
+    };
+
+    spliteasy-webclient = {
+      autoStart = true;
+      image = cfg.webclient.image;
+      extraOptions = [
+        "--pull=never"
+      ];
+      ports = ["${toString cfg.webclient.port}:80"];
+    };
   };
 
-  # Open firewall for the backend
-  networking.firewall.allowedTCPPorts = [ cfg.port ];
+  # Open firewall for both services
+  networking.firewall.allowedTCPPorts = [ cfg.backend.port cfg.webclient.port ];
 }
